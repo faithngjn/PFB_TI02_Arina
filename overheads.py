f70b5884ba4b9b56733dcefe5cef74abcbc2408d
namarrmann <158125483+namarrmann@users.noreply.github.com>
2024-02-01 10:54:54 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -17,8 +17,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     for row in reader:
         # Get the cash
         # and append to the deliveryRecords list
-        Overhead_data.append(float(row[1]))
-        Overhead_name.append(row[0])
+
 
 #Find the greatest overhead
 def compute_highest(Overhead_data, Overhead_name):

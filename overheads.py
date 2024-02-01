b43506645d48209ec47e804653dd3a6ca2057e18
faithngjn <156878089+faithngjn@users.noreply.github.com>
2024-02-01 21:39:05 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -2,29 +2,70 @@ import csv
 from pathlib import Path
 
 # Create a file path to the csv file.
-fp = Path.cwd() /"project_group"/"csv_reports"/ "Overheads.csv"
+fp = Path.cwd() / "Overheads.csv"
 
 # Read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+
+    # Create a CSV reader
     reader = csv.reader(file, delimiter=",")
-    next(reader) # Skip header
 
-    # Create an empty list for Overhead_data
-    Overhead_data = []
-    Overhead_name = []
-  
-    # Append cash-on-hand data into Overhead_data list
+    next(reader)  # Skip header
+
+    # Create lists for Overhead_data and Overhead_name
+    
+    # List to store numerical data for overheads
+    Overhead_data = []  
+
+    # List to store names of overhead categories
+    Overhead_name = [] 
+
+    # Iterate through each row in the CSV file
     for row in reader:
-        # Get the cash
-        # and append to the deliveryRecords list
+
+        # Add category name to the list
+        Overhead_name.append(row[0])  
+
+        # Add the amounts to the list as a float
+        Overhead_data.append(float(row[1])) 
+
+# Find the index of the maximum value in a list
+def find_index_of_max_value(first):
+    if not first:
+
+        # return none if list is empty
+        return None  
+    
+    # Find the maximum value in the list
+    max_value = max(first)  
 
 
-#Find the greatest overhead
+    # Iterate through the list to find the index of the maximum value
+    for index, value in enumerate(first):
+
+        if value == max_value:
+
+            # Return the index when the maximum value is found
+            return index  
+
+# Find the greatest overhead
 def compute_highest(Overhead_data, Overhead_name):
+
     "function will find the highest overhead and display the data"
-    highest_overhead = max(Overhead_data)
-    category_index = Overhead_data.index(highest_overhead)
+    
+    if not Overhead_data:
+
+        print("No data to compute highest overhead.")
+
+        return 
+
+    # Call the find_index_of_max_value function to get the index of the highest value
+    category_index = find_index_of_max_value(Overhead_data)
+
+    # Get the name of the category with the highest value
     category_name = Overhead_name[category_index].upper()
-    print(f"[HIGHEST OVERHEAD] {category_name}: {highest_overhead}")
+
+    # Print 
+    print(f"[HIGHEST OVERHEAD] {category_name}: {Overhead_data[category_index]}")
 
 compute_highest(Overhead_data, Overhead_name)
